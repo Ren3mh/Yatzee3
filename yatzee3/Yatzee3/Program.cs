0f1ffbfa359094a3df4e0394d68b1d3a37d01ac7
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-02-27 11:17:08 +0100

    Første udkast regler og antalspillere

--- yatzee3/Yatzee3/Program.cs
+++ yatzee3/Yatzee3/Program.cs
@@ -5,37 +5,43 @@
         static void Main(string[] args)
         {
 
-            //Renés roderi:
             Console.WriteLine("Hej nye spillere!");
 
-            Console.WriteLine("velkommen til yatzee!");
+            Console.WriteLine("Velkommen til yatzee!\n\n");
 
-            string[] scoreNAME = { "1'ere", "2'ere", "3'ere", "4'ere", }
+            Console.WriteLine("REGLER:\n" +
+                "\t1.En spiller slår med terningerne. \n\n" +
+                "\t2.Spilleren kan vælge at beholde et eller flere terninger og kaste de resterende igen. \n\n" +
+                "\t3.Spilleren har tre kast i alt til at opnå den ønskede kombination.\n\n" +
+                "\t4.Når spilleren er tilfreds med et kast, skal de vælge en kombination fra scorekortet og notere summen af de valgte terninger i feltet.\n\n" +
+                "\t5.En kombination kan kun bruges én gang.\n\n" +
+                "\t6.Spillet fortsætter med, at spillerne skiftes til at kaste terningerne.\n\n");
 
-            //Console.WriteLine("spiller 1, skriv dit navn og afslut med enter:");
-            //string spiller1 = Console.ReadLine();
+            Console.WriteLine("Hvor mange spillere er I?\n" +
+                "skriv 2 eller 3 og tryk enter");
+            int playerCount = int.Parse(Console.ReadLine());
 
-            //int[] s1Score = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+            //
+            switch(playerCount)
+            {
+                case 2:
+                    Console.WriteLine("Du har valgt 2 spillere");
+                        break;
+                case 3:
+                    Console.WriteLine("Du har valgt 3 spillere");
+                        break;
+                default:
+                    Console.WriteLine("Du skal skrive 2 eller 3 og derefter enter");
+                    break;
+                
+            }
 
-            //Console.WriteLine("spiller 2, skriv dit navn og afslut med enter:");
-            //string spiller2 = Console.ReadLine();
-
-            //int[] s2Score = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
 
 
+            //string[,] scoreBoard = { { 1, 4, 2 }, { 3, 6, 8 } };
+            // Kolonne højde er 17: 1 spillernavn, 14 score muligheder, 2 bonus
+            // Række længden er 3-4: 1 Score, 2-3 spillere (mindst 2 spillere)
 
-            //Console.WriteLine(spiller1 + " kast terningerne med enter!");
-            //Console.ReadLine();
-            //{
-            //    int terning1, terning2, terning3, terning4, terning5;
-            //    Random r = new Random();
-            //    terning1 = r.Next(1, 7);
-            //    terning2 = r.Next(1, 7);
-            //    terning3 = r.Next(1, 7);
-            //    terning4 = r.Next(1, 7);
-            //    terning5 = r.Next(1, 7);
-            //    Console.WriteLine(spiller1 + " du har slået: " + terning1 + ", " + terning2 + ", " + terning3 + ", " + terning4 + " & " + terning5);
-            //}
             Console.ReadLine();
         }
     }
