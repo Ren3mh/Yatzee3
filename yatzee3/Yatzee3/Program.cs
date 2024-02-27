9d7e801ceaf0154683e9ae02ee70d05e2a9bfbb2
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-02-27 10:33:17 +0100

    Udkommenteret Renés roderi

--- yatzee3/Yatzee3/Program.cs
+++ yatzee3/Yatzee3/Program.cs
@@ -5,16 +5,23 @@
         static void Main(string[] args)
         {
 
-            ////Renés roderi:
-            //Console.WriteLine("Hej nye spillere!");
+            //Renés roderi:
+            Console.WriteLine("Hej nye spillere!");
 
-            //Console.WriteLine("velkommen til yatzee!");
+            Console.WriteLine("velkommen til yatzee!");
+
+            string[] scoreNAME = { "1'ere", "2'ere", "3'ere", "4'ere", }
 
             //Console.WriteLine("spiller 1, skriv dit navn og afslut med enter:");
             //string spiller1 = Console.ReadLine();
 
+            //int[] s1Score = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+
             //Console.WriteLine("spiller 2, skriv dit navn og afslut med enter:");
             //string spiller2 = Console.ReadLine();
+
+            //int[] s2Score = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, };
+
 
 
             //Console.WriteLine(spiller1 + " kast terningerne med enter!");
@@ -22,14 +29,14 @@
             //{
             //    int terning1, terning2, terning3, terning4, terning5;
             //    Random r = new Random();
-            //    terning1 = r.Next(1, 6);
-            //    terning2 = r.Next(1, 6);
-            //    terning3 = r.Next(1, 6);
-            //    terning4 = r.Next(1, 6);
-            //    terning5 = r.Next(1, 6);
+            //    terning1 = r.Next(1, 7);
+            //    terning2 = r.Next(1, 7);
+            //    terning3 = r.Next(1, 7);
+            //    terning4 = r.Next(1, 7);
+            //    terning5 = r.Next(1, 7);
             //    Console.WriteLine(spiller1 + " du har slået: " + terning1 + ", " + terning2 + ", " + terning3 + ", " + terning4 + " & " + terning5);
             //}
-            //Console.ReadLine();
+            Console.ReadLine();
         }
     }
 }
