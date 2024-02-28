2f47acd5905d5212308bf2d2ba9d5333815c1289
René Mose Hansen <rmha63250@edu.ucl.dk>
2024-02-28 21:44:31 +0100

    metoder til 2 og 3 spiller scoreboard tilføjet samt forsøg på permanent scoreboard metode

--- yatzee3/Yatzee3/Program.cs
+++ yatzee3/Yatzee3/Program.cs
@@ -68,13 +68,55 @@
                     "\t2.Spilleren kan vælge at beholde et eller flere terninger og kaste de resterende igen. \n\n" +
                     "\t3.Spilleren har tre kast i alt til at opnå den ønskede kombination.\n\n" +
                     "\t4.Når spilleren er tilfreds med et kast, skal de vælge en kombination fra scorekortet \n" +
-                    "og notere summen af de valgte terninger i feltet.\n\n" +
+                    "\tog notere summen af de valgte terninger i feltet.\n\n" +
                     "\t5.En kombination kan kun bruges én gang.\n\n" +
                     "\t6.Spillet fortsætter med, at spillerne skiftes til at kaste terningerne.\n\n" +
                     "\tOg der er bonus ved 63: 50p og 93: 100p \n\n)");
             }
 
+            //Laver scoreboard med 3*17 til 2 spillere med score kategorier og spiller1 og spiller2, printer derefter til consol
+            public static void createScoreBoard2()
+            {
+                string[,] scoreBoardArr = new string[,]  { { "Score", "Player1", "Player2" }, { "1'ere", "", "" }, { "2'ere", "", "" }, { "3'ere", "", "" },
+                    { "4'ere", "", "" }, { "5'ere", "", "" }, { "6'ere", "", "" }, { "1 Par", "", "" }, { "2 Par", "", "" }, { "3 ens", "", "" }, 
+                    { "4 ens", "", "" }, { "Li straigth", "", "" }, { "St straight", "", "" }, { "Chancen", "", "" }, { "Yatzee", "", "" }, { "Bonus63", "", "" }, { "Bonus93", "", "" }, };
 
+                for (int i = 0; i < scoreBoardArr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < scoreBoardArr.GetLength(1); j++)
+                    {
+                        {
+                            System.Console.Write($"{scoreBoardArr[i, j]} ");
+                        }
+                        System.Console.Write("\t\t");
+                    }
+                    System.Console.WriteLine();
+                }
+
+
+            }
+
+            //Laver scoreboard med 4*17 til 3 spillere med score kategorier og spiller1, spiller2 og spiller3 printer derefter til consol
+            public static void createScoreBoard3()
+            {
+                string[,] scoreBoardArr = new string[,]  { { "Score", "Player1", "Player2", "Player3" }, { "1'ere", "", "", "" }, { "2'ere", "", "", "" }, { "3'ere", "", "", "" },
+                    { "4'ere", "", "", "" }, { "5'ere", "", "", "" }, { "6'ere", "", "", "" }, { "1 Par", "", "", "" }, { "2 Par", "", "", "" }, { "3 ens", "", "", "" },
+                    { "4 ens", "", "", "" }, { "Li straigth", "", "", "" }, { "St straight", "", "", "" }, { "Chancen", "", "", "" }, { "Yatzee", "", "", "" }, { "Bonus63", "", "", "" }, { "Bonus93", "", "", "" }, };
+
+                for (int i = 0; i < scoreBoardArr.GetLength(0); i++)
+                {
+                    for (int j = 0; j < scoreBoardArr.GetLength(1); j++)
+                    {
+                        {
+                            System.Console.Write($"{scoreBoardArr[i, j]} ");
+                        }
+                        System.Console.Write("\t\t");
+                    }
+                    System.Console.WriteLine();
+                }
+
+
+            }
 
             //// 1: lav scoreboard-array med playerCount 2: udfyld scoreNavne i søjle 0
             ////Scoreboard opstart, laver en 2-dimensionel array som har række længde: spillerantal + 1. og en kolonne højde: 17
@@ -82,6 +124,14 @@
             ////Række længden er 3 - 4: 1 Score, 2 - 3 spillere(mindst 2 spillere)
             //playerCount = playerCount + 1;
             //string[,] scoreBoard = new string[playerCount, 17];
+
+            //clearer console og indsætter scoreboard som det første så det altid står øverst/samme sted
+            public static void permScore()
+            {
+                Console.Clear();
+                Scoreboard.createScoreBoard2();
+               
+            }
         }
 
         static void Main(string[] args)
@@ -89,7 +139,7 @@
 
             Scoreboard.Velkomst();
 
-            //Spiller antal valg med do-while og switch til at vælge 2 eller 3 spillere
+            //Spiller antal valg med do-while og switch til at vælge 2 eller 3 spillere (burde måske laves med try/catch/exception)
             Console.WriteLine("Hvor mange spillere er I?\n" +
                 "skriv 2 eller 3 og tryk enter");
 
@@ -101,11 +151,11 @@
                 switch (playerCount)
                 {
                     case 2:
-                        Console.WriteLine("Du har valgt 2 spillere");
+                        Console.WriteLine("Du har valgt 2 spillere, tryk enter for at begynde...");
                         chooseAgain = 1;
                         break;
                     case 3:
-                        Console.WriteLine("Du har valgt 3 spillere");
+                        Console.WriteLine("Du har valgt 3 spillere, tryk enter for at begynde...");
                         chooseAgain = 1;
                         break;
                     default:
@@ -115,14 +165,22 @@
 
                 }
             } while (chooseAgain == 0);
+            
+            //enter eller whatever for continue
+            Console.ReadLine();
+            Console.Clear() ;
+
+            if (playerCount == 2)
+                { Scoreboard.createScoreBoard2(); }
+            else
+            { Scoreboard.createScoreBoard3(); }
+
+            Console.WriteLine("\n\nSpil din terning... Din terning blev... Hvor vil du gemme? Tryk enter");
+            Console.ReadLine() ;
 
 
-
-            //Scoreboard opstart, laver en 2-dimensionel array som har række længde: spillerantal + 1. og en kolonne højde: 17
-            //Kolonne højde er 17: 1 spillernavn, 14 score muligheder, 2 bonus
-            //Række længden er 3 - 4: 1 Score, 2 - 3 spillere(mindst 2 spillere)
-            playerCount = playerCount + 1;
-            string[,] scoreBoard = new string[playerCount, 17];
+            Scoreboard.permScore();
+            
 
             Console.ReadLine();
 
