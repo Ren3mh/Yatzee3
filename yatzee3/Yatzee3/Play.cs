9cf0f147a7fe430c6d6859529286bc81ac15cb0d
Sander Elgaard Andersen <sean62359@hotmail.edu.ucl.dk>
2024-03-01 00:01:02 +0100

    før Renes ændres

--- yatzee3/Yatzee3/Play.cs
+++ yatzee3/Yatzee3/Play.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Formats.Asn1.AsnWriter;
+﻿using static Yatzee3.Play;
 
 namespace Yatzee3
 {
@@ -27,7 +22,7 @@ namespace Yatzee3
 
         }
 
-        public static int Main(int turns = 3)
+        public static int Turn(Program.ScoreBoard scoreBoard, int turns = 3)
         {
             int score = 0;
 
@@ -58,7 +53,7 @@ namespace Yatzee3
                 }
             }
 
-            ChooseScore(dices);
+            score = Score.ChooseScore(dices);
 
             return score;
         }
@@ -123,29 +118,6 @@ namespace Yatzee3
             return dices;
         }
 
-        static void ChooseScore(Dice[] dices)
-        {
-            // mulige scores printes
-            Score.Kombinationer kombis = Score.FindScores(dices);
-
-            Console.WriteLine("Nu kan du vælge disse score: ");
-
-            Console.WriteLine("\t1'ere: " + kombis.EnEre);
-            Console.WriteLine("\t2'ere: " + kombis.ToEre);
-            Console.WriteLine("\t3'ere: " + kombis.TreEre);
-            Console.WriteLine("\t4'ere: " + kombis.FireEre);
-            Console.WriteLine("\t5'ere: " + kombis.FemEre);
-            Console.WriteLine("\t6'ere: " + kombis.SeksEre);
-            Console.WriteLine("\tPar: " + kombis.EtPar);
-            Console.WriteLine("\tTo Par: " + kombis.ToPar);
-            Console.WriteLine("\tTre Ens: " + kombis.TreEns);
-            Console.WriteLine("\tFire Ens: " + kombis.FireEns);
-            Console.WriteLine("\tLille Straight: " + kombis.LilleStraight);
-            Console.WriteLine("\tStor Straight: " + kombis.StorStraight);
-            Console.WriteLine("\tChancen: " + kombis.Chancen);
-            Console.WriteLine("\tYatzy: " + kombis.Yatzy);
-
-        }
     }
 
     internal class Score
@@ -165,163 +137,188 @@ namespace Yatzee3
             public int FireEns;
             public int LilleStraight;
             public int StorStraight;
-            public int Chancen;
-            public int Yatzy;
-        }
+            public int Chancen_;
+            public int Yatzy_;
 
-        public static int[] FindScores(Play.Dice[] dices_)
-        {
-            Kombinationer kombi = new Kombinationer();
-            int[] dices = dicesToArray(dices_);
-            int[] possibleScores = new int[14];
+            public int[] Scores = new int[14];
 
-            // find the best score each
-            possibleScores[0] = kombi.EnEre = SameNumber(dices, 1);
-            possibleScores[1] = kombi.ToEre = SameNumber(dices, 2);
-            possibleScores[2] = kombi.TreEre = SameNumber(dices, 3);
-            possibleScores[3] = kombi.FireEre = SameNumber(dices, 4);
-            possibleScores[4] = kombi.FemEre = SameNumber(dices, 5);
-            possibleScores[5] = kombi.SeksEre = SameNumber(dices, 6);
-            possibleScores[6] = kombi.EtPar = Pair(dices);
-            possibleScores[7] = kombi.ToPar = TwoPairs(dices);
-            possibleScores[8] = kombi.TreEns = FindxSame(dices, 3);
-            possibleScores[9] = kombi.FireEns = FindxSame(dices, 4);
-            possibleScores[10] = kombi.LilleStraight = Straight(dices, false);
-            possibleScores[11] = kombi.StorStraight = Straight(dices, true);
-            possibleScores[12] = kombi.Chancen = Chancen(dices);
-            possibleScores[13] = kombi.Yatzy = Yatzy(dices);
-
-            return possibleScores;
-
-            // tjek score ved 1'ere - 6'ere
-            int SameNumber(int[] dicesNumbers, int number)
+            public Kombinationer(Play.Dice[] dices_)
             {
-                int score = 0;
+                int[] dices = dicesToArray(dices_);
 
-                foreach (int diceNumber in dicesNumbers)
-                    if (diceNumber == number)
-                        score += diceNumber;
+                // find the best score each
+                Scores[0] = SameNumber(dices, 1);
+                Scores[1] = SameNumber(dices, 2);
+                Scores[2] = SameNumber(dices, 3);
+                Scores[3] = SameNumber(dices, 4);
+                Scores[4] = SameNumber(dices, 5);
+                Scores[5] = SameNumber(dices, 6);
+                Scores[6] = Pair(dices);
+                Scores[7] = TwoPairs(dices);
+                Scores[8] = FindxSame(dices, 3);
+                Scores[9] = FindxSame(dices, 4);
+                Scores[10] = Straight(dices, false);
+                Scores[11] = Straight(dices, true);
+                Scores[12] = Chancen(dices);
+                Scores[13] = Yatzy(dices);
 
-                return score;
-            }
+                EnEre = SameNumber(dices, 1);
+                ToEre = SameNumber(dices, 2);
+                TreEre = SameNumber(dices, 3);
+                FireEre = SameNumber(dices, 4);
+                FemEre = SameNumber(dices, 5);
+                SeksEre = SameNumber(dices, 6);
+                EtPar = Pair(dices);
+                ToPar = TwoPairs(dices);
+                TreEns = FindxSame(dices, 3);
+                FireEns = FindxSame(dices, 4);
+                LilleStraight = Straight(dices, false);
+                StorStraight = Straight(dices, true);
+                Chancen_ = Chancen(dices);
+                Yatzy_ = Yatzy(dices);
 
-            int Pair(int[] dicesNumbers, int used = -2)
-            {
-                Array.Reverse(dicesNumbers);
-                int score = 0;
-
-                // find bedste mulige par
-                for (int i = 0; i < dicesNumbers.Length; i++)
+                // tjek score ved 1'ere - 6'ere
+                int SameNumber(int[] dicesNumbers, int number)
                 {
-                    if (dicesNumbers[i] == used || dicesNumbers[i] == used + 1)
-                        continue;
+                    int score = 0;
 
-                    int posibleScore = 0;
-                    int number1 = dicesNumbers[i];
+                    foreach (int diceNumber in dicesNumbers)
+                        if (diceNumber == number)
+                            score += diceNumber;
 
-                    for (int j = i + 1; j < dices.Length; j++)
-                    {
-                        if (number1 == dicesNumbers[j])
-                            posibleScore = number1 * 2;
-                    }
-
-                    //se om paret er bedre
-                    if (posibleScore == 12)
-                        return 12;
-                    else if (posibleScore > score)
-                        score = posibleScore;
-                }
-                return score;
-            }
-
-            int TwoPairs(int[] dicesNumbers)
-            {
-                Array.Sort(dicesNumbers);
-                Array.Reverse(dicesNumbers);
-
-                int score = 0;
-
-                int par1Score = Pair(dicesNumbers);
-
-                // remove used numbers
-                int used = -2;
-                for (int i = 0; i < dices.Length; i++)
-                {
-                    if (dicesNumbers[i] == par1Score / 2)
-                    {
-                        used = i;
-                        break;
-                    }
+                    return score;
                 }
 
-                int par2Score = Pair(dicesNumbers, used);
-
-                return par1Score + par2Score;
-            }
-
-            int FindxSame(int[] dicesNumbers, int x)
-            {
-                int[] destinct = dicesNumbers.Distinct().ToArray();
-
-                foreach (int num in destinct)
+                int Pair(int[] dicesNumbers, int used = -2)
                 {
-                    int times = 0;
+                    Array.Reverse(dicesNumbers);
+                    int score = 0;
 
+                    // find bedste mulige par
                     for (int i = 0; i < dicesNumbers.Length; i++)
                     {
-                        if ((dicesNumbers[i] == num))
-                            times++;
+                        if (dicesNumbers[i] == used || dicesNumbers[i] == used + 1)
+                            continue;
 
-                        if (times == x)
-                            return times * num;
+                        int posibleScore = 0;
+                        int number1 = dicesNumbers[i];
+
+                        for (int j = i + 1; j < dices.Length; j++)
+                        {
+                            if (number1 == dicesNumbers[j])
+                                posibleScore = number1 * 2;
+                        }
+
+                        //se om paret er bedre
+                        if (posibleScore == 12)
+                            return 12;
+                        else if (posibleScore > score)
+                            score = posibleScore;
                     }
+                    return score;
                 }
-                return 0;
-            }
 
-            int Straight(int[] dicesNumbers, bool stor)
-            {
-                int[] destinct = dicesNumbers.Distinct().ToArray();
+                int TwoPairs(int[] dicesNumbers)
+                {
+                    Array.Sort(dicesNumbers);
+                    Array.Reverse(dicesNumbers);
 
-                if (destinct.Length < 4)
+                    int score = 0;
+
+                    int par1Score = Pair(dicesNumbers);
+
+                    // remove used numbers
+                    int used = -2;
+                    for (int i = 0; i < dices.Length; i++)
+                    {
+                        if (dicesNumbers[i] == par1Score / 2)
+                        {
+                            used = i;
+                            break;
+                        }
+                    }
+
+                    int par2Score = Pair(dicesNumbers, used);
+
+                    return par1Score + par2Score;
+                }
+
+                int FindxSame(int[] dicesNumbers, int x)
+                {
+                    int[] destinct = dicesNumbers.Distinct().ToArray();
+
+                    foreach (int num in destinct)
+                    {
+                        int times = 0;
+
+                        for (int i = 0; i < dicesNumbers.Length; i++)
+                        {
+                            if ((dicesNumbers[i] == num))
+                                times++;
+
+                            if (times == x)
+                                return times * num;
+                        }
+                    }
                     return 0;
+                }
 
-                else if (destinct[0] == 6 && stor == true)
-                    return 20;
+                int Straight(int[] dicesNumbers, bool stor)
+                {
+                    int[] destinct = dicesNumbers.Distinct().ToArray();
 
-                else if (destinct[0] == 5 && stor == false)
-                    return 15;
+                    if (destinct.Length < 4)
+                        return 0;
 
-                else
-                    return 0;
+                    else if (destinct[0] == 6 && stor == true)
+                        return 20;
+
+                    else if (destinct[0] == 5 && stor == false)
+                        return 15;
+
+                    else
+                        return 0;
+
+                }
+
+                int Chancen(int[] dicesNumbers)
+                {
+                    int score = 0;
+                    foreach (int num in dicesNumbers) { score += num; }
+                    return score;
+                }
+
+                int Yatzy(int[] dicesNumbers)
+                {
+                    int[] destinct = dicesNumbers.Distinct().ToArray();
+                    if ((destinct.Length > 1))
+                        return 0;
+                    else
+                        return 50;
+                }
+
+                int[] dicesToArray(Play.Dice[] dices)
+                {
+                    int[] diceNumbers = new int[dices.Length];
+                    for (int i = 0; i < dices.Length; i++)
+                        diceNumbers[i] = dices[i].diceNumber;
+                    Array.Sort(diceNumbers); // sorterer dem lav->høj
+
+                    return diceNumbers;
+                }
 
             }
+        }
 
-            int Chancen(int[] dicesNumbers)
-            {
-                int score = 0;
-                foreach (int num in dicesNumbers) { score += num; }
-                return score;
-            }
+        public static int ChooseScore(Dice[] dices)
+        {
+            Kombinationer kombinationer = new Kombinationer(dices);
 
-            int Yatzy(int[] dicesNumbers)
-            {
-                int[] destinct = dicesNumbers.Distinct().ToArray();
-                if ((destinct.Length > 1))
-                    return 0;
-                else
-                    return 50;
-            }
+            Program.ScoreBoard.
 
-            int[] dicesToArray(Play.Dice[] dices)
-            {
-                int[] diceNumbers = new int[dices.Length];
-                for (int i = 0; i < dices.Length; i++)
-                    diceNumbers[i] = dices[i].diceNumber;
-                Array.Sort(diceNumbers); // sorterer dem lav->høj
+            Console.WriteLine("Dine mulige Scores:");
 
-                return diceNumbers;
-            }
+            
 
         }
     }
